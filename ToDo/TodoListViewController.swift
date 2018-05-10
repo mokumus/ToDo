03a5dd2b8f83fcfaf0879523d06a7956071b70a2
Muhammed Okumuş <rocketpolka@gmail.com>
2018-05-10 22:53:06 +0300

    Add new functionality complete

--- ToDo/TodoListViewController.swift
+++ ToDo/TodoListViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-	let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+	var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -52,6 +53,31 @@ class TodoListViewController: UITableViewController {
 			cell.accessoryType = .checkmark
 		}
 	}
-
+	
+	//MARK - Add New Items
+	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+		let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+		
+		var textField = UITextField()
+		
+		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+			// What will happen when addItem is clicked on UIAlert
+			if textField.text?.count != 0 {
+				self.itemArray.append(textField.text!)
+			}
+			self.tableView.reloadData()
+		}
+		
+		alert.addTextField { (alertTextField) in
+			alertTextField.placeholder = "Create new item"
+			textField = alertTextField
+		}
+		
+		alert.addAction(action)
+		present(alert, animated: true, completion: nil)
+		
+		
+		
+	}
 }
 
